0009825ba0bcf08802fa0373a352d46667c8fbe4
CWH617 <33533512+CWH617@users.noreply.github.com>
2018-02-02 09:50:23 +0800

    Add files via upload

---
+++ src/main/java/RentalCalculator.java
@@ -0,0 +1,56 @@
+
+
+public class RentalCalculator {
+
+	public static double carRental(String carModel, int duration, boolean firstTimer, boolean member){
+		
+		double rentalFees = 0.0;
+				
+		if (firstTimer){ //first rental
+			if (carModel.equals("Honda Fit"))
+				rentalFees = 65.0*duration*0.95; //honda fit
+			else
+				rentalFees = 80.0*duration*0.95; //toyota wish
+		}
+		else if (member){ //subsequent rental 
+			if (carModel.equals("Honda Fit") && (duration > 3))
+				rentalFees = 65.0*duration*0.80;
+			else if (carModel.equals("Toyota Wish") && (duration > 3))
+				rentalFees = 80.0*duration*0.80;
+			else if (carModel.equals("Honda Fit") && (duration < 3))
+				rentalFees = 65.0*duration*0.90;
+			else if (carModel.equals("Toyota Wish") && (duration < 3))
+				rentalFees = 80.0*duration*0.90;
+			
+		}
+		else {
+			if(carModel.equals("Toyota Wish")) {
+			rentalFees = 80.0*duration; //toyota wish
+			}
+			else {
+				rentalFees = 65.0*duration; //honda fit
+			}
+		}
+			
+		return rentalFees;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Honda, firsttime, 1D : " + RentalCalculator.carRental("Honda Fit", 1, true, false));
+		System.out.println("Toyota, firsttime, 1D : " + RentalCalculator.carRental("Toyota Wish", 1, true, false));
+		System.out.println("Honda, firsttime, 1D : " + RentalCalculator.carRental("Honda Fit", 1, false, true));
+		System.out.println("Toyota, firsttime, 1D : " + RentalCalculator.carRental("Toyota Wish", 1, false, true));
+		
+		System.out.println("Honda, firsttime, 5D : " + RentalCalculator.carRental("Honda Fit", 5, false, false));
+		System.out.println("Toyota, firsttime, 5D : " + RentalCalculator.carRental("Toyota Wish", 5, false, false));
+		System.out.println("Honda, firsttime, 5D : " + RentalCalculator.carRental("Honda Fit", 5, false, true));
+		System.out.println("Toyota, firsttime, 5D : " + RentalCalculator.carRental("Toyota Wish", 5, false, true));
+	//	System.out.println("Honda, subsequent, 5D : " + RentalCalculator.carRental("Honda Fit", 5, false));
+	//	System.out.println("Toyota, subsequent, 5D : " + RentalCalculator.carRental("Toyota Wish", 5, false));
+	//	System.out.println("Honda, subsequent, 3D : " + RentalCalculator.carRental("Honda Fit", 3, false));
+	//	System.out.println("Toyota, subsequent, 3D : " + RentalCalculator.carRental("Toyota Wish", 3, false));
+	}
+}
+
